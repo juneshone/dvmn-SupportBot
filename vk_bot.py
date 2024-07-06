778c51c1f1942123d6a918a8cdfadabb9ec6934b
juneshone <shamrova.julia@mail.ru>
2024-07-06 15:42:56 +0300

    Echobot for VK

--- vk_bot.py
+++ vk_bot.py
@@ -1,20 +1,29 @@
-import vk_api
+import random
+import vk_api as vk
 
 from environs import Env
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
 
 
 def main():
     env = Env()
     env.read_env()
-    vk_session = vk_api.VkApi(token=env.str('VK_GROUP_TOKEN'))
-
+    vk_session = vk.VkApi(token=env.str('VK_GROUP_TOKEN'))
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW:
             print('Новое сообщение:')
             if event.to_me:
+                echo(event, vk_api)
                 print('Для меня от: ', event.user_id)
             else:
                 print('От меня для: ', event.user_id)
