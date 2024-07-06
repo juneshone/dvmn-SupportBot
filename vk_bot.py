cf51e0ec0c8414e17533333755fe8890213fcde1
juneshone <shamrova.julia@mail.ru>
2024-07-06 17:45:42 +0300

    Add DialogFlow to VK bot

--- vk_bot.py
+++ vk_bot.py
@@ -4,17 +4,24 @@ import vk_api as vk
 from environs import Env
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-def echo(event, vk_api):
+from dialogflow import detect_intent_text
+
+
+def intent(event, vk_api):
+    text = detect_intent_text(
+        project_id=env.str('PROJECT_ID'),
+        session_id=event.user_id,
+        text=event.text,
+        language_code='ru'
+    )
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=text,
         random_id=random.randint(1,1000)
     )
 
 
 def main():
-    env = Env()
-    env.read_env()
     vk_session = vk.VkApi(token=env.str('VK_GROUP_TOKEN'))
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
@@ -23,7 +30,7 @@ def main():
         if event.type == VkEventType.MESSAGE_NEW:
             print('Новое сообщение:')
             if event.to_me:
-                echo(event, vk_api)
+                intent(event, vk_api)
                 print('Для меня от: ', event.user_id)
             else:
                 print('От меня для: ', event.user_id)
@@ -31,4 +38,6 @@ def main():
 
 
 if __name__ == '__main__':
+    env = Env()
+    env.read_env()
     main()
